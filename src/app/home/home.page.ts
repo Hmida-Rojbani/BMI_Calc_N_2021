6e0b34c5b07f9608cf31c033f100ab0dba959029
Hmida Rojbani <31728681+Hmida-Rojbani@users.noreply.github.com>
2021-09-21 19:46:03 +0100

    final-steps

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -9,12 +9,34 @@ export class HomePage {
   height: number;
   weight: number;
   bmi: number;
+  description: string;
+  imgSrc: string;
 
   constructor() {}
 
+  selectDescription(){
+    const prefixe = '../../assets/bmi/';
+    if (this.bmi < 18.5) {
+      this.description = 'underweight';
+      this.imgSrc = prefixe+'underweight.png';
+    } else if (this.bmi < 25) {
+      this.description = 'normal';
+      this.imgSrc = prefixe+'normal.png';
+    } else if (this.bmi < 30) {
+      this.description = 'overweight';
+      this.imgSrc = prefixe+'overweight.png';
+    }else if (this.bmi < 35) {
+      this.description = 'obese';
+      this.imgSrc = prefixe+'obese.png';
+    } else {
+      this.description = 'exteremly obese';
+      this.imgSrc = prefixe+'ext_obese.png';
+    }
+  }
+
   bmiCalculation(){
     this.bmi = this.weight / (this.height*this.height)*10000;
-    console.log('bmi', this.bmi);
+    this.selectDescription();
   }
 
 }
